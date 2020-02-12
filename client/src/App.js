3bc5d265f9a611d8acf7a9a0ee1b94cc266480c7
Kevetic <kevetic@outlook.com>
2020-02-11 18:44:12 -0600

    done

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/nav";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <div>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Search} />
+              <Route path="/search" component={Search} />
+              <Route exact path="/saved" component={Saved} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/searchDiv.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const styles = {
+  boop: {
+    backgroundColor: "darkgrey",
+    padding: "1em"
+  }
+}
+
+function SearchDiv(props) {
+  return (
+    <div style={styles.boop}>
+      <h3>Book Search</h3>
+      <form>
+        <div className="form-group">
+          <label for="bookInput">Book</label>
+          <input
+            type="text"
+            className="form-control"
+            id="bookInput"
+            placeholder="Ex. Harry Potter"
+            onChange={props.handleInputChange}
+            value={props.search}
+          />
+        </div>
+        <button onClick={props.handleFormSubmit} type="submit" className="btn btn-primary">Search</button>
+      </form>
+    </div>
+  )
+}
+
+export default SearchDiv;
